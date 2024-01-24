bd8c9269ebed001d555d8aa6f7178d23a24d312a
Thunderblade73 <gaidermarkus@gmail.com>
2024-01-24 21:24:06 +0100

    Revert "capsuled GuiContainerEvents"
    This reverts commit 0349f2b110390f1dce7ca228e8529e5daf476fda.

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiContainerHook.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiContainerHook.kt
@@ -6,7 +6,6 @@ import at.hannibal2.skyhanni.events.GuiContainerEvent.CloseWindowEvent
 import at.hannibal2.skyhanni.events.GuiContainerEvent.SlotClickEvent
 import at.hannibal2.skyhanni.test.SkyHanniDebugsAndTests
 import net.minecraft.client.gui.inventory.GuiContainer
-import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.inventory.Slot
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
@@ -19,41 +18,29 @@ class GuiContainerHook(guiAny: Any) {
     }
 
     fun closeWindowPressed(ci: CallbackInfo) {
-        GlStateManager.pushMatrix()
         if (CloseWindowEvent(gui, gui.inventorySlots).postAndCatch()) ci.cancel()
-        GlStateManager.popMatrix()
     }
 
     fun backgroundDrawn(mouseX: Int, mouseY: Int, partialTicks: Float) {
         if (!SkyHanniDebugsAndTests.globalRender) return
-        GlStateManager.pushMatrix()
         GuiContainerEvent.BackgroundDrawnEvent(gui, gui.inventorySlots, mouseX, mouseY, partialTicks).postAndCatch()
-        GlStateManager.popMatrix()
     }
 
     fun foregroundDrawn(mouseX: Int, mouseY: Int, partialTicks: Float) {
-        GlStateManager.pushMatrix()
         GuiContainerEvent.ForegroundDrawnEvent(gui, gui.inventorySlots, mouseX, mouseY, partialTicks).postAndCatch()
-        GlStateManager.popMatrix()
     }
 
     fun onDrawSlot(slot: Slot, ci: CallbackInfo) {
-        GlStateManager.pushMatrix()
         val event = GuiContainerEvent.DrawSlotEvent.GuiContainerDrawSlotPre(gui, gui.inventorySlots, slot)
         if (event.postAndCatch()) ci.cancel()
-        GlStateManager.popMatrix()
     }
 
     fun onDrawSlotPost(slot: Slot) {
-        GlStateManager.pushMatrix()
         GuiContainerEvent.DrawSlotEvent.GuiContainerDrawSlotPost(gui, gui.inventorySlots, slot).postAndCatch()
-        GlStateManager.popMatrix()
     }
 
     fun onMouseClick(slot: Slot?, slotId: Int, clickedButton: Int, clickType: Int, ci: CallbackInfo) {
-        GlStateManager.pushMatrix()
         if (SlotClickEvent(gui, gui.inventorySlots, slot, slotId, clickedButton, clickType).postAndCatch()) ci.cancel()
-        GlStateManager.popMatrix()
     }
 
     fun onDrawScreenAfter(
@@ -61,8 +48,6 @@ class GuiContainerHook(guiAny: Any) {
         mouseY: Int,
         ci: CallbackInfo,
     ) {
-        GlStateManager.pushMatrix()
         if (DrawScreenAfterEvent(mouseX, mouseY, ci).postAndCatch()) ci.cancel()
-        GlStateManager.popMatrix()
     }
 }
